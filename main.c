0227297245c0ddc92b51abcb5994105c7fcf3fda
doncigma <astroboy772@gmail.com>
2024-11-07 18:12:01 -0800

    arch

--- main.c
+++ main.c
@@ -1,5 +1,18 @@
+typedef struct {
+    char* key;
+    float val;
+} pair;
+
+typedef struct {
+    char* key;
+    pair* val;
+} element;
 
 int main(int argc, char *argv[]) {
-
+    element table[] = {
+        {"foo", 10}, {"bar", 42}, {"bazz", 36}, {"buzz", 7},
+        {"bob", 11}, {"jane", 100}, {"x", 200}};
+    size_t num_items = sizeof(table) / sizeof(table);
+    
     return 0;
 }
