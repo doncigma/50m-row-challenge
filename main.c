b9a2c1d55543ab737eb3a03902ecb1671d080743
doncigma <astroboy772@gmail.com>
2024-11-08 16:03:09 -0800

    test cities

--- main.c
+++ main.c
@@ -1,18 +1,37 @@
-typedef struct {
-    char* key;
-    float val;
-} pair;
+#include <stdio.h>
+#include <stddef.h>
+#include <string.h>
 
 typedef struct {
     char* key;
-    pair* val;
-} element;
+    float sum;
+    int cnt;
+    float avg;
+    float min;
+    float max;
+} tempObj;
+
+typedef struct {
+    char* key;
+    tempObj temps;
+} city;
+
+void addTemp(float temp, string cityName) {
+    this = table[cityName]
+    this.sum += temp
+    this.cnt += 1
+    temp < this.min ? this.min = temp : nothing
+    temp > this.max ? this.max = temp : nothing
+}
 
 int main(int argc, char *argv[]) {
-    element table[] = {
+    city cities[] = {
         {"foo", 10}, {"bar", 42}, {"bazz", 36}, {"buzz", 7},
         {"bob", 11}, {"jane", 100}, {"x", 200}};
-    size_t num_items = sizeof(table) / sizeof(table);
+    
+    size_t length = sizeof(cities) / sizeof(cities);
+
+    
     
     return 0;
 }
