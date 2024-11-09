f1753886013a40ac85c9df5559e47f1700607373
doncigma <astroboy772@gmail.com>
2024-11-08 17:09:21 -0800

    parsing

--- main.c
+++ main.c
@@ -13,25 +13,58 @@ typedef struct {
 
 typedef struct {
     char* key;
-    tempObj temps;
+    tempObj tempData;
+    city* next;
 } city;
 
-void addTemp(float temp, string cityName) {
-    this = table[cityName]
-    this.sum += temp
-    this.cnt += 1
-    temp < this.min ? this.min = temp : nothing
-    temp > this.max ? this.max = temp : nothing
+city* search(const city* cities, const size_t size, const char* key) {
+    for (size_t i=0; i<size; i++) {
+        if (strcmp(cities[i].key, key) == 0) {
+            return &cities[i];
+        }
+    }
+    return NULL;
 }
 
 int main(int argc, char *argv[]) {
-    city cities[] = {
-        {"foo", 10}, {"bar", 42}, {"bazz", 36}, {"buzz", 7},
-        {"bob", 11}, {"jane", 100}, {"x", 200}};
+    const char const* fileName = argv[1];
     
-    size_t length = sizeof(cities) / sizeof(cities);
+    city cities[1000];
+    size_t size = sizeof(cities) / sizeof(cities);
+    
+    
+    
+    char cityName[] = "";
+    float temp;
 
+    FILE* file = fopen(fileName, "r");
+    char line[40];
     
-    
+    while (fgets(line, sizeof(line), file)) {
+        char currChar = line[0];
+        char str[] = currChar;
+        str + currChar;
+        
+        // We want to go until we see a semicolon and add along the way for the city name, store
+        for (int i = 0; currChar != ";"; i++) {
+            currChar = line[i];
+        }
+        // Then we want to go until the null termiantor for the temp, convert then store
+        for (int i = 0; currChar != "\n"; i++) {
+            currChar = line[i];
+        }
+
+        city* this;
+        if (this = search(&cities, size, &cityName)) {
+            tempObj thisData = this->tempData;
+            
+            thisData.sum += temp;
+            thisData.cnt++;
+            temp < thisData.min ? thisData.min = temp : thisData.min;
+            temp > thisData.max ? thisData.max = temp : thisData.max;
+        }
+        continue;
+    }
+
     return 0;
 }
