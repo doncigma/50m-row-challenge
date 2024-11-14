e9fca745b7f8b43da2729f1808cb369771568a7a
doncigma <astroboy772@gmail.com>
2024-11-13 18:47:44 -0800

    mmap

--- Makefile
+++ Makefile
@@ -1,2 +1,24 @@
-process_measurements:
-	gcc -g -o main main.c
+# Compiler and compiler flags
+CC = gcc
+CFLAGS = -Wall
+
+# Targets
+all: main
+
+# Default target (builds without extra flags)
+main: main.c
+	$(CC) $(CFLAGS) -o main main.c
+
+# Compile with optimization level O2
+o2: main.c
+	$(CC) $(CFLAGS) -O2 -o main main.c
+
+time:
+	time ./main
+
+mem:
+	valgrind --leak-check=full ./main
+
+# Clean up generated files
+clean:
+	rm -f main main_O2 main_debug main_debug_O2
--- main.c
+++ main.c
@@ -7,7 +7,6 @@
 #include <unistd.h>
 #include <sys/stat.h>
 
-#define LINELENGTH 106
 #define TABLESIZE 1000
 #define CITYNAMELENGTH 101
 
@@ -31,31 +30,28 @@ typedef struct {
     size_t tempsizeof;
 } citytable;
 
-#define FNVPRIME 
+#define FNVPRIME 16777619
 #define FNVOFSSET 2166136261U
 
 /// @brief FNV-1a hash implementation.
 /// @param key String to hash.
 /// @return The hashed value divided by table size.
-unsigned long hash(const char* key) {
-    unsigned long hash = 2166136261U; // fnv offset basis
+int hash(const char* key) {
+    unsigned long hash = FNVOFSSET;
     unsigned char letter;
 
-    while(letter = *key++) {
+    while((letter = *key++)) {
         hash ^= letter;
-        hash *= 16777619; // fnv prime
+        hash *= FNVPRIME;
     }
 
     return hash % TABLESIZE;
 }
 
 city* search(citytable* const table, const char* const key) {
-    // for (city* iter = table->cities; iter != table->endptr; iter++)
-    //     if (strcmp(iter->key, key) == 0) return iter;
-    // return NULL;
     city* found = &table->cities[hash(key)];
-    if (strcmp(found->key, key) != 0) found = NULL;
-    return found;
+    if (found && (strcmp(found->key, key) != 0) ) return found;
+    return NULL;
 }
 
 /// @brief Attempts to add a city to the table. If key is already in the table, it updates its values with new temp. Otherwise, it constructs a new city and appends.
@@ -73,24 +69,22 @@ void add(citytable* const table, char* const key, const float temp) {
         found->tempData.cnt += 1;
         if (temp < found->tempData.min) found->tempData.min = temp;
         else if (temp > found->tempData.max) found->tempData.max = temp;
-        // return found;
     }
     // Make new city and append if not in table
     else {
-        table->size += 1;
-        table->endptr = &table->cities[table->size - 1];
-       
         // COLLISIONS
 
-        // city* cityadd = &table->cities[table->size - 1];
-        city* cityadd = &table->cities[hash(key)];
-        strncpy(cityadd->key, key, CITYNAMELENGTH);
+        int index = hash(key);
+
+        city* cityadd = &table->cities[index];
+        strcpy(cityadd->key, key);
         cityadd->tempData.sum = temp;
         cityadd->tempData.cnt = 1;
         cityadd->tempData.min = temp;
         cityadd->tempData.max = temp;
 
-        // return cityadd;
+        table->size += 1;
+        table->endptr = &table->cities[table->size - 1];
     }
 }
 
@@ -105,16 +99,13 @@ int main(int argc, char *argv[]) {
     const char* fileName = "measurements.txt";
     const char* ofileName = "output.txt";
 
-    // FILE* infile = fopen(fileName, "r");
-    // if (!infile) { fprintf(stderr, "Err: Input file could not open. Check name and extension.\n"); return 1; }
-
     int fd = open(fileName, O_RDONLY);
     if (fd == -1) { fprintf(stderr, "Err: Input file could not open. Check name and extension.\n"); return 1; }
 
     struct stat st;
     if (fstat(fd, &st) == -1) { fprintf(stderr, "Err: Could not retrieve input file metadata.\n"); close(fd); return 1; }
     
-    float* input = (float*)mmap(NULL, st.st_size, PROT_READ, MAP_SHARED, fd, 0);
+    char* input = (char*)mmap(NULL, st.st_size, PROT_READ, MAP_SHARED, fd, 0);
     if (input == (void*)-1) { fprintf(stderr, "Mmap failed.\n"); close(fd); return 1; }
     
     citytable table;
@@ -125,28 +116,37 @@ int main(int argc, char *argv[]) {
 
     // Pointer math to parse the city name and temp by the semicolon
     int i = 0;
-    char line[LINELENGTH];
-    // for fgets write a custom one because we know size of line (buffer to store to) LINELENGTH * sizeof(string)
-    // while (fgets(line, LINELENGTH, infile)) {
-    while (read(fd, line, sizeof(line)) > 0) {
-        char* delim = strpbrk(line, ";");
-        if (!delim) { fprintf(stderr, "Err: No city found. Line: %d.\n", i); i++; continue; }
+    char* currline = input;
+    char* end = input + st.st_size;
+    while (currline != end) {
+        char* newline = strpbrk(currline, "\n");
+        if (!newline) { currline = end; continue; }
+
+        size_t len = newline - currline; // excludes newline char
+        char addline[len + 1];
+        memcpy(addline, currline, len);
+        addline[len] = '\0';
+
+        char* delim = strpbrk(addline, ";");
+        if (!delim) { fprintf(stderr, "Err: No city found. Skipping line: %d.\n", i); currline = newline + 1; i++; continue; }
         
         *delim = '\0';
         float temp = strtof(delim + 1, NULL);
-        add(&table, line, temp);
-        // city* tmp = add(&table, line, strtof(delim + 1, NULL));
-        // if (!tmp) { fprintf(stderr, "Err: Problem allocating memory while adding. Line: %d.\n", i); continue; }
+        add(&table, addline, temp);
+
+        currline = newline + 1;
         i++;
     }
 
     munmap(input, st.st_size);
     close(fd);
-    // fclose(infile);
 
     // Calculate and Output
     FILE* ofile = fopen(ofileName, "w");
     if (!ofile) { fprintf(stderr, "Err: Output file could not open. Check name and extension.\n"); return 1; }
+
+    // int fd = open(ofileName, O_WRONLY);
+    // if (fd == -1) { fprintf(stderr, "Err: Output file could not open. Check name and extension.\n"); return 1; }
 
     for (int i = 0; i < table.size; i++) {
         char* key = table.cities[i].key;
@@ -156,6 +156,8 @@ int main(int argc, char *argv[]) {
         double avg = data.sum / data.cnt;
 
         fprintf(ofile, "%s,%0.1f,%0.1lf,%0.1f\n", key, data.min, avg, data.max);
+        // char line[] = ""
+        // write(fd, );
     }
 
     fclose(ofile);
